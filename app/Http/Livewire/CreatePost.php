265e374c9c395b253dab22a18de6585af1ecd135
JuanCardozo <juanguillermopc@gmail.com>
2021-05-12 12:58:12 -0500

    added loadingStates

--- app/Http/Livewire/CreatePost.php
+++ app/Http/Livewire/CreatePost.php
@@ -5,20 +5,40 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use \App\Models\Post;
 
+
 class CreatePost extends Component
 {
 
     public $open = false;
     public $title, $content;
 
+    protected $rules = [
+        'title'   => 'required|max:20',
+        'content' => 'required|min:30',
+    ];
+
+    protected $messages = [
+        'title.required' => 'Titulo requerido.',
+        'title.max'       => 'Titulo debe ser maximo de 20 caracteres.',
+    ];
+
+
+    public function updated($propiedad)
+    {
+        $this->validateOnly($propiedad);
+    }// updated
+
+
+
     public function save(){
+
+        $this->validate();
         Post::create([
             'title' => $this->title,
             'content' => $this->content,
         ]);
 
         $this->reset(['title','content','open']);
-
         //
         $this->emit('alert', 'El post se creo correctamente');
         $this->emitTo('show-posts','render');
--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -14,11 +14,14 @@
         <x-slot name="content">
             <div class="mb-4">
                 <x-jet-label>Titulo del Post</x-jet-label>
-                <x-jet-input type="text" class="w-full" wire:model.defer="title"/>       
+                <x-jet-input type="text" class="w-full" wire:model="title" />
+                <x-jet-input-error for="title" />
             </div>
+
             <div class="mb-4">
                 <x-jet-label>Contenido del Post</x-jet-label>
-                <textarea class="form-control w-full" rows="6" wire:model.defer="content"></textarea>      
+                <textarea class="form-control w-full" rows="6" wire:model.defer="content"></textarea>
+                <x-jet-input-error for="content" />
             </div>
 
         </x-slot>
@@ -26,9 +29,12 @@
 
         <x-slot name="footer">
             <x-jet-secondary-button wire:click="$set('open',false)">Cancelar</x-jet-secondary-button>
-            <x-jet-danger-button wire:click="save()">
+            <x-jet-danger-button wire:click="save">
                 <i class="fas fa-plus-circle mr-2"></i>Crear
             </x-jet-danger-button>
+            <span wire:loading wire:target="save">
+                Procesando...
+            </span>
         </x-slot>
 
     </x-jet-dialog-modal>
